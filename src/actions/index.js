74e7e9688f0a6fc56e2db1e4a243a00fc0d112e6
M4T <mateo.lesiuk@gmail.com>
2019-04-03 17:25:32 +0200

    Project setup with Redux

---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+import {
+  CREATE_NOTE,
+  FETCH_NOTE,
+  FETCH_NOTES,
+  EDIT_NOTE,
+  DELETE_NOTE,
+} from './types';
+import notes from '../apis/notes';
+
+export const createNote = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await notes.post(`/${userId}/notes/create`, {
+    ...formValues,
+  });
+
+  dispatch({
+    type: CREATE_NOTE,
+    payload: response.data,
+  });
+};
+
+export const fetchNote = id => async (dispatch, getState) => {
+  // const { userId } = getState().auth;
+  const response = await notes.get(`/abc123/notes/${id}`);
+
+  dispatch({
+    type: FETCH_NOTE,
+    payload: response.data,
+  });
+};
