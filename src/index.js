350cab3490a638ad8303826505e70119b74f89b3
M4T <mateo.lesiuk@gmail.com>
2019-04-05 09:43:07 +0200

    Add composeEnhancer to redux store to connect Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
 import Router from './components/Router';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
