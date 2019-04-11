4bd8291f95513bd4735521bdf3245dc0e273ae6e
M4T <mateo.lesiuk@gmail.com>
2019-04-11 23:00:23 +0200

    Move create and edit note logic/component to separate components

---
+++ src/components/CreateNote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CreateNote extends React.Component {
+  render() {
+    return <div>Create Note</div>;
+  }
+}
+
+export default CreateNote;
---
+++ src/components/EditNote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class EditNote extends React.Component {
+  render() {
+    return <div>Edit Note</div>;
+  }
+}
+
+export default EditNote;
