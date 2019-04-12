2c1b962d862623b5d4af9c5ae43cddc4aa33bd00
M4T <mateo.lesiuk@gmail.com>
2019-04-12 09:41:24 +0200

    Add useEffect hook which makes async calls to API

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-class EditNote extends React.Component {
-  render() {
-    return <div>Edit Note</div>;
-  }
-}
+// Action Creator
+import { fetchNote } from '../actions';
 
-export default EditNote;
+// Components
+import Form from './Form';
+
+const EditNote = props => {
+  const { id } = props.match.params;
+
+  const onSubmit = formValues => {
+    console.log(formValues);
+  };
+  /* eslint-disable */
+  useEffect(() => {
+    async function fetch() {
+      await props.fetchNote(id);
+    }
+
+    fetch();
+  }, [id]);
+  /* eslint-enable */
+
+  return (
+    <div>
+      Edit Note
+      <Form
+        onSubmit={onSubmit}
+        initialValues={_.pick(props.note, 'title', 'note')}
+      />
+    </div>
+  );
+};
+
+const mapStateToProp = state => ({
+  note: state.notes.selectedNote,
+});
+
+export default connect(
+  mapStateToProp,
+  { fetchNote }
+)(EditNote);
