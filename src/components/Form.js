a718dc871cfabc22e844d25d8e4d186fc7e0c5c0
M4T <mateo.lesiuk@gmail.com>
2019-04-23 22:51:18 +0200

    Change form inputs render, add form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,10 +11,11 @@ const Container = styled.form`
   /* height: 100%; */
 `;
 
-const Title = styled(Field).attrs({
-  component: 'input',
-  placeholder: 'Title',
-})`
+const FieldContainer = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
   display: inline-block;
   margin: 5rem auto 0;
   width: 60%;
@@ -30,19 +31,20 @@ const Title = styled(Field).attrs({
     font-size: 1.8rem;
     letter-spacing: 0.1em;
   }
+
+  &[data-error='true'] {
+    border-color: red;
+  }
 `;
 
-const Note = styled(Field).attrs({
-  component: 'textarea',
-  placeholder: 'Note',
-})`
+const TextArea = styled.textarea`
   display: block;
   margin: 2.5rem auto 0;
   width: 60%;
   min-height: 30rem;
   padding: 0.5rem 1rem;
   font-size: 1.8rem;
-  box-shadow: 0.15rem 0.5rem 1rem rgba(0, 0, 0, 0.25);
+  box-shadow: 0.15rem 0.15rem 0.5rem 0.2rem rgba(0, 0, 0, 0.25);
   border: none;
   border-radius: 0.5rem;
   resize: vertical;
@@ -51,6 +53,23 @@ const Note = styled(Field).attrs({
     font-size: 1.8rem;
     letter-spacing: 0.1em;
   }
+
+  &[data-error='true'] {
+    box-shadow: 0.15rem 0.15rem 0.5rem 0.2rem rgba(255, 0, 0, 0.5);
+  }
+`;
+
+const ErrorMessage = styled.span`
+  position: absolute;
+  left: 50%;
+  bottom: -2rem;
+  transform: translateX(-50%);
+  width: 60%;
+  display: block;
+  color: red;
+  text-align: left;
+  font-size: 14px;
+  font-weight: bold;
 `;
 
 const Buttons = styled.div`
@@ -66,11 +85,47 @@ class Form extends React.Component {
     this.props.onSubmit(formValues);
   };
 
+  required = value => (value ? undefined : 'Required');
+
+  renderField = ({ input, placeholder, type, name, meta }) => (
+    <FieldContainer>
+      {input.name === 'title' ? (
+        <Input
+          {...input}
+          placeholder={placeholder}
+          name={name}
+          type={type}
+          data-error={!!(meta.touched && meta.error)}
+        />
+      ) : (
+        <TextArea
+          {...input}
+          placeholder={placeholder}
+          name={name}
+          data-error={!!(meta.touched && meta.error)}
+        />
+      )}
+      {meta.touched &&
+        (meta.error && <ErrorMessage>{meta.error}</ErrorMessage>)}
+    </FieldContainer>
+  );
+
   render() {
     return (
       <Container onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Title name="title" type="text" required />
-        <Note name="note" required />
+        <Field
+          name="title"
+          type="text"
+          placeholder="Title"
+          component={this.renderField}
+          validate={this.required}
+        />
+        <Field
+          name="note"
+          placeholder="Note"
+          component={this.renderField}
+          validate={this.required}
+        />
         <Buttons>
           <Button btnType="submit" content="Save" bgColor="#4285F4" />
           {_.has(this.props.initialValues, 'id') ? (
