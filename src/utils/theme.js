b4221ed4edce8f3039f902a7c265ed12fd6ef3e6
M4T <mateo.lesiuk@gmail.com>
2019-05-12 00:28:06 +0200

    Add color from whole app

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,16 @@
 const theme = {
   main: '#4285f4',
+  lighterMain: '#5c9fff',
+  lightGrey: '#f7f7f7',
+  borderColor: 'rgba(0, 0, 0, 0.25)',
+  errorColor: 'rgba(255, 0, 0, 0.5)',
+  grey: '#5b5b5b',
+  white: '#fff',
+  noteHover: 'rgba(66, 133, 244, 0.2)',
+  green: '#06c73a',
+  shadowColor: 'rgba(88, 88, 88, 0.3)',
+  confirmBtnColor: '#ff6961',
+  cancelBtnColor: '#585858',
 };
 
 export default theme;
