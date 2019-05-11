e083c83fc60577570a9fdf4cc386822982a0e300
M4T <mateo.lesiuk@gmail.com>
2019-05-11 23:10:37 +0200

    Add NODE_ENV check to enable redux devtools only in development state

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import Router from './components/Router';
 import rootReducer from './reducers';
 import GlobalStyles from './utils/globalStyles';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
 
 const store = createStore(
   rootReducer,
