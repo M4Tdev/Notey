9b8c97ccb8d06693284bf7b3206d2bff53347454
M4T <mateo.lesiuk@gmail.com>
2019-05-13 20:03:52 +0200

    Implement mobile burger menu, add state and some methods for burger menu

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import TopBar from './TopBar';
 import NotesList from './NotesList';
 import Editor from './Editor';
 import Footer from './Footer';
+import BurgerMenu from './BurgerMenu';
 
 const Container = styled.div`
   width: 100vw;
@@ -19,9 +20,49 @@ const InnerContainer = styled.div`
   height: calc(100% - 6.5rem);
   display: flex;
   flex-direction: row;
+
+  @media ${props => props.theme.mediaQueries.smallest} {
+    display: block;
+  }
 `;
 
 class App extends React.Component {
+  state = {
+    mobile: null,
+    menuOpen: false,
+  };
+
+  componentDidMount() {
+    this.changeMobile();
+    window.addEventListener('resize', this.changeMobile);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.changeMobile);
+  }
+
+  changeMobile = () => {
+    // check if passed media query matches with window dimensions
+    window.matchMedia('(max-width: 37.5em)').matches
+      ? this.setState({ mobile: true })
+      : this.setState({ mobile: false });
+  };
+
+  handleStateChange = state => {
+    this.setState({ menuOpen: state.isOpen });
+  };
+
+  checkRoute = () => {
+    if (this.props.location.pathname !== '/notes') {
+      return true;
+    }
+    return false;
+  };
+
+  showMenu = () => {
+    this.setState({ menuOpen: true });
+  };
+
   onSignOut = async () => {
     await base.auth().signOut();
   };
@@ -35,8 +76,14 @@ class App extends React.Component {
       <Container>
         <TopBar userEmail={this.props.userEmail} onSignOut={this.onSignOut} />
         <InnerContainer>
-          <NotesList />
-          <Editor />
+          <NotesList isMobile={this.state.mobile} showMenu={this.showMenu} />
+          {this.state.mobile ? (
+            /* eslint-disable */
+            <BurgerMenu width={ '100%' } isOpen={ this.state.menuOpen } onStateChange={this.handleStateChange} />
+            /* eslint-enable */
+          ) : (
+            <Editor isMobile={this.state.mobile} />
+          )}
         </InnerContainer>
         <Footer />
       </Container>
